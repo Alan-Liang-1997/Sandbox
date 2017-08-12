8597bb5f8b7cc201277b697405fee97c4e9bddd3
Qoehliang <qoehliang@gmail.com>
2017-08-12 21:16:55 +1000

    Add name printing program

--- prac_02/ascii_table.py
+++ prac_02/ascii_table.py
@@ -13,8 +13,9 @@ print("The character for {} is {}".format(number, ascii_char))
 
 column = 0
 number_of_columns = int(input("Enter number of columns between 0 and 95: "))
-while number < 0 or number > 95:
+while number_of_columns < 0 or number_of_columns > 95:
     print("Invalid number of columns")
+print("Column Number : ASCII Number : ASCII Character")
 for i in range(0, number_of_columns, 1):
     column = column + 1
     ascii_char = chr(i+33)
