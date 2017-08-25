4dc0287657f4bb77a97807d3126586e1511ae439
Qoehliang <qoehliang@gmail.com>
2017-08-25 22:45:53 +1000

    Complete occurrence_of_words task

--- prac_05/occurrence_of_words.py
+++ prac_05/occurrence_of_words.py
@@ -1,6 +1,20 @@
 """
 CP1404/CP5632 Practical
-Count same words from input
+Count same words from input.
 """
+counted_words = {}
+text = input("Text: ")
+text = text.split()
 
-words_to_count = input("Text: ")
+for word in text:
+    if word in counted_words:
+        counted_words[word] += 1
+    else:
+        counted_words[word] = 1
+
+sorted_words = list(counted_words.keys())
+sorted_words.sort()
+longest_word = max(len(word) for word in counted_words)
+
+for word in sorted_words:
+    print("{:<{}} : {}".format(word, longest_word, counted_words[word]))
