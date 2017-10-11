8c3acda52dd724e5266190b992bec8f3bca9d377
Qoehliang <qoehliang@gmail.com>
2017-10-11 23:10:16 +1000

    Complete Walkthrough Example - Renaming Files

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -2,7 +2,7 @@
 CP1404/CP5632 Practical
 File renaming and os examples
 """
-import shutil
+# import shutil
 import os
 
 
@@ -32,7 +32,6 @@ def main():
             # shutil.move(filename, 'temp/' + new_name)
 
             # Processing subdirectories using os.walk()
-
             # os.chdir('..')  # .. means "up" one directory
             # for dir_name, subdir_list, file_list in os.walk('.'):
             #     print("In", dir_name)
@@ -43,12 +42,29 @@ def main():
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
     # First, replace the spaces and .TXT (the easy part)
-    filename = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = new_name[0].upper() + new_name[1:]
 
-    new_name = ""
-    # TODO: step-by-step, consider the problem cases and solve them
+    # Second, replace PascalCase. i.e. SilentNight
+    pascal_name = new_name[0]
+    for index, character in enumerate(new_name[1:]):
+        # If current character is uppercase, and the previous character is lowercase and not a underscore
+        if character.isupper() and new_name[index-1].islower() and not new_name[index] == "_":
+            pascal_name += "_" + character
+        else:
+            pascal_name += character
 
-    return new_name
+    # Third, replace names not in TitleCase. i.e. O little town of bethlehem
+    title_name = pascal_name[0]
+    for index, character in enumerate(pascal_name[1:]):
+        # Check if character is lowercase and if character is _
+        if character.islower() and pascal_name[index] == "_":
+            title_name += character.upper()
+        else:
+            title_name += character
+    # All cases have been considered, output final_name
+    final_name = title_name
+    return final_name
 
 
 main()
